eff5f0c359f164a8980b6f1396a6d6b77671d5fe
phpjishu <phpjishu@qq.com>
2018-09-14 07:34:29 +0800

    使用Xinyi.js统一引入VUX组件，防止权限组件内部不可用

--- src/router/index.js
+++ src/router/index.js
@@ -19,7 +19,7 @@ routerdata.forEach(function (obj, index) {
   if (index === 0) {
     router.addRoutes([{
       path: '/',
-      name: "home",
+      name: 'home',
       component: resolve => require(['../' + obj], resolve),
       meta: {
         config: require('../' + obj + '.json')
