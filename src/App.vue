2189e901879a43d3ebfdc0c819a8bbc881202744
phpjishu <phpjishu@qq.com>
2018-09-14 10:11:25 +0800

    新增图标实例

--- src/App.vue
+++ src/App.vue
@@ -134,6 +134,7 @@
       left: 0;
       right: 0;
       bottom: 0;
+      overflow-y: auto;
     }
   }
 </style>
--- src/pages/example/icons.vue
+++ src/pages/example/icons.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-    <group-title>ICONS</group-title>
-    <grid :cols="4">
-      <grid-item :label="item" v-for="(item,index) in icons" :key="index">
-        <xy-icon :value="item"></xy-icon>
-      </grid-item>
-    </grid>
+    <group>
+      <cell-box is-link>
+        <a href="https://material.io/icons/" target="_blank">查看所有图标</a>
+      </cell-box>
+    </group>
+    <div v-for="(item,index) in iconsDefault.categories">
+      <group-title>{{item.name}}</group-title>
+      <group>
+        <grid :cols="3">
+          <grid-item :label="item.id" v-for="(item,index) in item.icons" :key="index">
+            <xy-icon slot="icon" :value="item.id" color="#999"></xy-icon>
+          </grid-item>
+        </grid>
+      </group>
+    </div>
+
   </div>
 </template>
 
@@ -14,7 +24,8 @@
     name: 'iconsView',
     data() {
       return {
-        icons: ['more_horiz', 'more_horiz', 'more_horiz', 'more_horiz', 'more_horiz', 'more_horiz', 'more_horiz']
+        icons: ['more_horiz', 'more_horiz', 'more_horiz', 'more_horiz', 'more_horiz', 'more_horiz', 'more_horiz'],
+        iconsDefault: require('./data/icons.json')
       }
     }
   }
